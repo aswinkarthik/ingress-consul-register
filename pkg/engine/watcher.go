82bcf567d3b5684eafc6c0fac31851d65a8e9bb1
aswinkarthik93 <aswinkarthik93@gmail.com>
2018-03-18 12:42:43 +0530

    Coupling tags with ipaddress to be registered with consul

--- pkg/engine/watcher.go
+++ pkg/engine/watcher.go
@@ -8,13 +8,19 @@ import (
 	"github.com/ericchiang/k8s/apis/extensions/v1beta1"
 )
 
+type consulService struct {
+	Tags      []string
+	IpAddress string
+}
+
 func RunOnce() {
 	log.Println("Initiating first run")
 	ingresses := fetchIngresses()
 	tags := retrieveTags(ingresses)
-	utils.PrettyPrint(tags)
 	service := fetchControllerService()
-	utils.PrettyPrint(service)
+	c := &consulService{Tags: tags, IpAddress: service.getIpAddress()}
+	log.Println("Following service will be registered in consul")
+	utils.PrettyPrint(c)
 }
 
 func retrieveTags(ingresses []*v1beta1.Ingress) []string {
